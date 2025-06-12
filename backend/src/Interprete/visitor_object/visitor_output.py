77c7ef435fdefbe47540285926b3cb493f39893f
Eduardo Lau <136769670+Eduardo-Lau@users.noreply.github.com>
2025-06-11 22:54:14 -0600

    Add files via upload

--- backend/src/Interprete/visitor_object/visitor_output.py
+++ backend/src/Interprete/visitor_object/visitor_output.py
@@ -1,4 +1,12 @@
 from backend.src.Interprete.semanticas.semanticaSuma import validar_suma
+from backend.src.Interprete.semanticas.semanticaPotencia import validar_potencia
+from backend.src.Interprete.semanticas.semanticaMayorIgualQue import validar_Mayorigualque
+from backend.src.Interprete.semanticas.semanticaMenorIgualQue import validar_Menorigualque
+from backend.src.Interprete.semanticas.semanticaModulo import validar_modulo
+from backend.src.Interprete.semanticas.semanticaMayorQue import validar_Mayorque
+from backend.src.Interprete.semanticas.semanticaNot import validar_Not
+from backend.src.Interprete.semanticas.semanticaXor import validar_Xor
+from backend.src.Interprete.semanticas.semanticaUmenos import validar_Umenos
 from backend.src.Interprete.visitor_object.visitorBase import Visitor
 from backend.src.Interprete.nodes.Nodo import Nodo
 from backend.src.Interprete.simbol.RaizArbol import Arbol
@@ -44,19 +52,40 @@ class Visitor_Output(Visitor):
     
     def visit_Potencia(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
-        return valorIzq ** valorDer
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
+        resultado, tipo = validar_potencia(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_Umenos(self, nodo: Nodo):
         valor = nodo.expresion.accept(self)
-        return -valor
+        #SE COMPRUEBA SI EL VALOR ES UN ERROR
+        if isinstance(valor, Error):
+            return valor
+        resultado, tipo = validar_Umenos(nodo.expresion, valor)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_Modulo(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
         if valorDer == 0:
             raise ZeroDivisionError("División por cero no permitida.")
-        return valorIzq % valorDer
+        resultado, tipo = validar_modulo(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_IgualQue(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
@@ -70,13 +99,29 @@ class Visitor_Output(Visitor):
     
     def visit_MayorQue(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
-        return valorIzq > valorDer
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
+        resultado, tipo = validar_Mayorque(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_MayorIgualQue(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
-        return valorIzq >= valorDer
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
+        resultado, tipo = validar_Mayorigualque(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_MenorQue(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
@@ -85,8 +130,16 @@ class Visitor_Output(Visitor):
     
     def visit_MenorIgualQue(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
-        return valorIzq <= valorDer
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
+        resultado, tipo = validar_Menorigualque(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
     
     def visit_And(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
@@ -100,12 +153,24 @@ class Visitor_Output(Visitor):
     
     def visit_Not(self, nodo: Nodo):
         valor = nodo.expresion.accept(self)
-        return not valor
+        if isinstance(valor, Error):
+            return valor
+        resultado, tipo = validar_Not(nodo.expresion, valor)
+        nodo.tipo = tipo
+        return resultado
     
     def visit_Xor(self, nodo: Nodo):
         valorIzq = nodo.izquierda.accept(self)
+        #SE COMPRUEBA SI EL VALOR IZQUIERDO ES UN ERROR
+        if isinstance(valorIzq, Error):
+            return valorIzq
         valorDer = nodo.derecha.accept(self)
-        return (valorIzq and not valorDer) or (not valorIzq and valorDer)
+        #SE COMPRUEBA SI EL VALOR DERECHO ES UN ERROR
+        if isinstance(valorDer, Error):
+            return valorDer
+        resultado, tipo = validar_Xor(nodo.izquierda, nodo.derecha, valorIzq, valorDer)
+        nodo.tipo = tipo  # Actualiza el tipo del nodo
+        return resultado
 
     def visit_Println(self, nodo: Nodo):
         valor = nodo.expresion.accept(self)
